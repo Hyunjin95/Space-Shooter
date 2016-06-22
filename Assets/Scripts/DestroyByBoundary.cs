2f68d0f30301b68846d708a1994e3b034307f4e2
Hyunjin Jeong <das01063@snu.ac.kr>
2016-06-22 19:11:36 +0900

    Add asteroids

--- Assets/Scripts/DestroyByBoundary.cs
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -3,6 +3,6 @@ using System.Collections;
 
 public class DestroyByBoundary : MonoBehaviour {
 	void OnTriggerExit(Collider other) {
-		Destroy (other.gameObject);
+		Destroy (other.gameObject);	
 	}
 }
